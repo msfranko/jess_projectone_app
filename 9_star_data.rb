09808701d203ee16ca43097eec427ebff6b982db
msfranko <msfranko.jf@gmail.com>
2014-09-05 15:49:53 -0400

    committed

---
+++ 9_star_data.rb
@@ -0,0 +1,20 @@
+nine_years = [1901, 19010, 1919, 1928]
+eight_years = [1902, 1911, 1920, 1929]
+seven_years = [1903, 1912, 1921, 1930]
+six_years = [1904, 1913, 1922, 1931]
+five_years = [1905, 1914, 1923, 1932]
+four_years = [1906, 1915, 1924, 1933]
+three_years = [1907, 1916, 1925, 1934]
+two_years = [1908, 1917, 1926, 1935]
+one_years = [1909, 1918, 1927, 1936]
+
+nine = {Nine Fire: Lover}
+eight = {Eight Earth: Transformer}
+seven = {Seven Metal: Artist}
+six = {Six Metal: Father}
+five = {Five Earth: Center}
+four  = {Four Wood: Guide}
+three = {Three Wood: Warrior}
+two = {Two Earth: Mother}
+one = {One Water: Innovator}
+
