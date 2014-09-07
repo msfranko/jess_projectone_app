47396203cbaf70e4a6b68ce935f527561ce210ea
msfranko <msfranko.jf@gmail.com>
2014-09-07 10:50:28 -0400

    remove years array, use method

--- 9_star_data.rb
+++ 9_star_data.rb
@@ -1,15 +1,15 @@
 require 'pry'
 
 years_array = [
-{ :year => 1901, :number => 9, :element => "fire" },
-{ :year => 1902, :number => 8, :element => "earth" },
-{ :year => 1903, :number => 7, :element => "metal" },
-{ :year => 1904, :number => 6, :element => "metal" },
-{ :year => 1905, :number => 5, :element => "earth" },
-{ :year => 1906, :number => 4, :element => "wood" },
-{ :year => 1907, :number => 3, :element => "wood" },
-{ :year => 1908, :number => 2, :element => "earth" },
-{ :year => 1909, :number => 1, :element => "water" },
+{ :1901 => 9, :element => "fire" },
+{ :1902 => 8, :element => "earth" },
+{ :1903 => 7, :element => "metal" },
+{ :1904 => 6, :element => "metal" },
+{ :1905 => 5, :element => "earth" },
+{ :1906 => 4, :element => "wood" },
+{ :1907 => 3, :element => "wood" },
+{ :1908 => 2, :element => "earth" },
+{ :1909 => 1, :element => "water" },
 ]
 
 # nine = {Nine Fi re: Lover}
@@ -29,5 +29,22 @@ def get_first_number
   end
 end
 
-def describe_element
+def year_ki(year)
+if year > 1899 && year < 2000
+  four_digit_array = year.to_s.split("")
+  three_digit_array = four_digit_array.delete_at(0)
+  two_digit_array = three_digit_array.delete_at(0)
+  value1 = two_digit_array[0].to_i
+  value2 = two_digit_array[1].to_i
+  value3 = value1 + value2
+  if value3 < 10
+    value4 = 10 - value3
+  if value3 > 10
+    value4 = value3.to_s.split("")
+
+
+
+
+
+# def describe_element
 
